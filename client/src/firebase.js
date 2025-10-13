e845e11ce5af1ced3284a9bce260a633c6c698fc
Shashank Atmakur <134150598+ShashankAtmakur@users.noreply.github.com>
2025-10-13 19:20:57 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWTxkgcyJrqzabhi5TjKfFD8Sr02XZl-Q",
-  authDomain: "socialex-85cb3.firebaseapp.com",
-  projectId: "socialex-85cb3",
-  storageBucket: "socialex-85cb3.appspot.com",
-  messagingSenderId: "682531867137",
-  appId: "1:682531867137:web:b9e07d8af4bcad5ba64e00"
+  apiKey: "your-firebase-apikey",
+  authDomain: "your-firebase-authDomain",
+  projectId: "your-firebase-projectId",
+  storageBucket: "your-firebase-storageBucket",
+  messagingSenderId: "your-firebase-messagingSenderId",
+  appId: "your-firebase-appId"
 };
 
 // Initialize Firebase
